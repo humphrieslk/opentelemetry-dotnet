ecb8660c2ab8d5d2fbc2b732367233556fb81e95
Laura Humphries <lhumphries@google.com>
2020-11-02 22:29:54 +0000

    Add explicit, linear, and exponential histograms

---
+++ src/OpenTelemetry/Metrics/Histogram/DoubleExponentialHistogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OpenTelemetry.Metrics.Histogram
+{
+    public class DoubleExponentialHistogram : ExponentialHistogram<double>
+    {
+        public DoubleExponentialHistogram(double scale, double growthFactor, int numberOfFiniteBuckets)
+            : base(scale, growthFactor, numberOfFiniteBuckets)
+        {
+        }
+
+        protected override int GetBucketIndex(double valueToAdd)
+        {
+            var doubleIndex = Math.Log(valueToAdd / this.Scale, this.GrowthFactor);
+
+            return (int)Math.Floor(doubleIndex);
+        }
+
+        protected override double GetHighestBound()
+        {
+            return this.Scale * Math.Pow(this.GrowthFactor, this.NumberOfFiniteBuckets);
+        }
+    }
+}
---
+++ src/OpenTelemetry/Metrics/Histogram/Int64ExponentialHistogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OpenTelemetry.Metrics.Histogram
+{
+    public class Int64ExponentialHistogram : ExponentialHistogram<long>
+    {
+        public Int64ExponentialHistogram(long scale, long growthFactor, int numberOfFiniteBuckets)
+            : base(scale, growthFactor, numberOfFiniteBuckets)
+        {
+        }
+
+        protected override int GetBucketIndex(long valueToAdd)
+        {
+            var doubleIndex = Math.Log((double)valueToAdd / this.Scale, this.GrowthFactor);
+
+            return (int)Math.Floor(doubleIndex);
+        }
+
+        protected override long GetHighestBound()
+        {
+            return this.Scale * (long)Math.Pow(this.GrowthFactor, this.NumberOfFiniteBuckets);
+        }
+    }
+}
